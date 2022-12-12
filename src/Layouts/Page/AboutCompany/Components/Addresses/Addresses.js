ed61802033ffd57a1622807f3d9b74b386c72055
catedots <120114400+catedots@users.noreply.github.com>
2022-12-12 22:07:19 +0000

    update address frame

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -4,7 +4,7 @@ import ListElement from "./ListElement";
 function Addresses() {
 
     return (
-        <div className='d-inline-flex flex-column gap-2 mt-4'>
+        <div className='d-inline-flex flex-column gap-2 mt-4 address-frame'>
             <div className='title'>Адреса заведений</div>
             <div className='d-inline-flex flex-column gap-3'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
