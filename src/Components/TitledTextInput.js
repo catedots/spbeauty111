c884d6f635e7adf8f303273fed98f184e838c554
catedots <120114400+catedots@users.noreply.github.com>
2022-12-12 21:02:01 +0000

    update text inputs

--- src/Components/TitledTextInput.js
+++ src/Components/TitledTextInput.js
@@ -1,22 +1,31 @@
 import styled from "styled-components";
 
+const TitleS = styled.div`
+    color: #94A3B8;
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 12px;
+`;
+
 const Input = styled.input`
-    outline: none;
-    border-radius: 0px;
-    border: solid #152842 1px;
-    background-color: #D9E2EF;
-    color: #666787;
-    &:hover {
-        background-color: #D9E2EFB8;
+    /* Steel Gray / 50 */
+    background: #F8FAFC;
+    /* Steel Gray / 200 */
+    border: 1px solid #E2E8F0;
+    border-radius: 24px;
+&:hover {
+        opacity 0.8;
     }
 `;
 
 function TitledTextInput({title, def}) {
-    const Title = title === undefined ? 
-        (<div></div>) : 
-        (<div>{title}</div>);
+    const Title = title === undefined 
+        ? (<div></div>) 
+        : (<TitleS>{title}</TitleS>);
     return (
-        <div className='d-flex flex-column gap-2'>
+        <div className='d-flex flex-column gap-0'>
             {Title}
             <Input className='p-2' defaultValue={def} ></Input>
         </div>
