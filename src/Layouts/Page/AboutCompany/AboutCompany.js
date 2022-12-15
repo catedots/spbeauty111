3a06ff0546779b6888d5b663977b48d5fe4447ce
catedots <120114400+catedots@users.noreply.github.com>
2022-12-15 18:01:31 +0000

    upd p

--- src/Layouts/Page/AboutCompany/AboutCompany.js
+++ src/Layouts/Page/AboutCompany/AboutCompany.js
@@ -7,11 +7,11 @@ function AboutCompany() {
             <Heading/> {/* Заголовок и кнопка справа */}
 
             <div class="container">
-  <div class="row justify-content-evenly">
-    <div class="col-4">
+  <div class="row justify-content-between">
+    <div class="col-5 w-50 p-3">
     <General/> {/* Основная информация вроде названия, типа и телефона */}
     </div>
-    <div class="col-4">
+    <div class="col-5 w-50 p-1">
     <Addresses/> {/* Список доступных адресов заведений */}
     </div>
   </div>  </div>  </div>
