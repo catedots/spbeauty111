55f897dcfc608f69a811195e4ef166a6feae983d
catedots <120114400+catedots@users.noreply.github.com>
2022-12-15 18:13:03 +0000

    upd addresses and le

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -3,12 +3,13 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-
+<div class="container justify-content-between">
         <div className='d-inline-flex flex-column gap-2 mt-5 address-frame col'>
             <div className='title'>Адреса заведений</div>
             <div className='d-inline-flex flex-column gap-3 my-4'>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
+                </div>
                 <div className='d-inline-flex flex-column gap-3 mt-5'>
                 <SecondaryButton>Добавить филиал</SecondaryButton> </div>
             </div> </div>
