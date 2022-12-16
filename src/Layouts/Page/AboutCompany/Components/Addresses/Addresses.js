4a4b3f4e2dd669deaf7012ec5234cd7b14076325
catedots <120114400+catedots@users.noreply.github.com>
2022-12-16 20:39:54 +0000

    upd addresses frame

--- src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
+++ src/Layouts/Page/AboutCompany/Components/Addresses/Addresses.js
@@ -3,16 +3,17 @@ import SecondaryButton from "../../../../../Components/SecondaryButton";
 import ListElement from "./ListElement";
 function Addresses() {
     return (
-<div class="container justify-content-between col">
-        <div className='d-flex flex-column gap-2 mt-5 address-frame'>
-            <div className='title'>Адреса заведений</div>
-            <div className='d-flex flex-column gap-3 my-4'>
+        <div className='d-flex justify-content-between col flex-column gap-2 mt-5 address-frame h-100'>
+      
+            <div className='d-flex flex-column gap-3'>
+            <div className='title mb-2'>Адреса заведений</div>
                 <ListElement>Санкт-Петербург, Невский пр-т. 28</ListElement>
                 <ListElement>Санкт-Петербург, Казанская ул, 7</ListElement>
                 </div>
-                <div className='d-flex flex-column gap-3 mt-5'>
+                
+                <div className='d-flex flex-column'>
                 <SecondaryButton>Добавить филиал</SecondaryButton> </div>
-            </div> </div>
+            </div> 
       
     );
 }
